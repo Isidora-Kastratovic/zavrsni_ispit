9e6492d319985347884cfd37561a68bae1ab47ab
Isidora Kastratovic <isidora-kastratovic@oykos-hub.com>
2020-06-13 14:54:57 +0200

    Initial commit

---
+++ zavrsni_ispit.js
@@ -0,0 +1,12 @@
+
+
+function myfunction(){
+	var a = document.getElementsByClassName('input1')[0];
+	alert(a.value);
+}
+
+
+function myfunction1(){
+	var b = document.getElementsByClassName('input2')[0];
+	alert(b.value);
+}
